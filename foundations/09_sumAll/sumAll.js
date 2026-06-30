86fdd10c3954ee07d5da545a6b72a6a3848eafa5
josh.i.. <35184227+himanshuhjoshi@users.noreply.github.com>
2026-06-30 10:34:07 +0530

    Completed arrays and loops

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,23 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    if ((num1<0 || num2<0) 
+        || (isNaN(num1) || isNaN(num2))
+        || (!Number.isInteger(num1) || !Number.isInteger(num2))
+    ) 
+    return "ERROR";
 
+    let max = num1, min = num2;
+    if(num1 < num2){
+        max = num2;
+        min = num1;
+    }
+
+    let sum = 0;
+
+    for(let i=min; i<=max; i++){
+        sum += i;
+    }
+
+    return sum;
 };
 
 // Do not edit below this line
